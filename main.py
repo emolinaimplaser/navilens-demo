67f3a4b925c8359f8c44bc2f93984e6c6f0867ae
emolinaimplaser <122015364+emolinaimplaser@users.noreply.github.com>
2023-01-05 13:27:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
